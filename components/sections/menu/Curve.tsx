41a4827ff77b240d71cc87f90b8f048910b8e8bf
Nabin <nabin.magar05r@gmail.com>
2026-03-29 18:41:23 +0545

    Fixed Framer motion ease type errors

--- components/sections/menu/Curve.tsx
+++ components/sections/menu/Curve.tsx
@@ -1,19 +1,21 @@
 "use client";
 import { motion } from "framer-motion";
+
 export const Curve = () => {
   const initialPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q-100 ${window.innerHeight/2} 100 0`
   const targetPath = `M100 0 L200 0 L200 ${window.innerHeight} L100 ${window.innerHeight} Q100 ${window.innerHeight/2} 100 0`
+  
   const pathAnimation = {
     initial: {
-        d: initialPath
+      d: initialPath
     },
     enter: {
-        d: targetPath,
-        transition: {duration: 1, ease: [0.76, 0, 0.24, 1]}
+      d: targetPath,
+      transition: { duration: 1, ease: [0.76, 0, 0.24, 1] as [number, number, number, number] }
     },
     exit: {
-        d: initialPath,
-        transition: {duration: 0.8, ease: [0.76, 0, 0.24, 1]}
+      d: initialPath,
+      transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] as [number, number, number, number] }
     }
   }
   
