078da7ed680862e7e667eb07a74062ae2adb5e37
Nabin <nabin.magar05r@gmail.com>
2026-04-01 16:53:46 +0545

    added 100svh to the Preloader

--- components/ui/PreLoader.tsx
+++ components/ui/PreLoader.tsx
@@ -79,7 +79,8 @@ export const PreLoader = () => {
           variants={slideUp}
           initial = 'initial'
           exit='exit'
-          className="w-screen bg-[#162734] fixed inset-0 z-[9999] text-white flex items-center justify-center"
+          className="w-screen bg-[#162734] fixed top-0 left-0 z-[9999] text-white flex items-center justify-center"
+          style={{ height: '100svh' }}
         >
             <motion.p
                 variants={opacity} 
