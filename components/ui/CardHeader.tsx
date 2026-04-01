000c4dc5207f84feab2dffa469f13644bd72e732
Nabin <nabin.magar05r@gmail.com>
2026-04-01 16:00:36 +0545

    added margin top on card header p tag in mobile view

--- components/ui/CardHeader.tsx
+++ components/ui/CardHeader.tsx
@@ -7,7 +7,7 @@ export const CardHeader = ({ title, description, className } : { title:string, d
         <StarIcon className="size-9 text-emerald-300 card-header-star" />
         <h3 className="font-display text-3xl">{title}</h3>
       </div>
-      <p className="text-sm lg:text-base max-w-xs text-white/60">
+      <p className="text-sm lg:text-base mt-2 md:mt-0 max-w-xs text-white/60">
         {description}
       </p>
     </div>
